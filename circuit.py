dd6f740c0ac307b97f88c6b298d6f5b7c1fb58b3
Amelia Fargason <amelia.fargason@gmail.com>
2026-04-13 18:21:06 -0400

    Initialize quantum circuit
    Work in progress

---
+++ circuit.py
@@ -0,0 +1,30 @@
+from qiskit import QuantumRegister, QuantumCircuit, ClassicalRegister
+from qiskit.circuit.library import UnitaryGate
+import numpy as np
+
+# Create a circuit with a register of three qubits
+# change this to actual A later
+
+# beginning dimension
+m_0 = 2 # of variables
+v_0 = np.random.rand(m_0)
+v_0_norm = np.linalg.norm(v_0)
+n_0 = int(np.ceil(np.log2(m_0)))
+print(n_0)
+A = np.eye(2**(n_0*3))
+
+qreg_prev = QuantumRegister(n_0)
+qreg_next = QuantumRegister(n_0)
+creg = ClassicalRegister(n_0)
+qreg_a = QuantumRegister(n_0)
+
+circ = QuantumCircuit(creg,qreg_prev,qreg_next,qreg_a)
+circ.prepare_state(v_0/v_0_norm,qubits=qreg_next)
+A=UnitaryGate(A, label='A')
+circ.append(A, range(n_0*3))
+circ.x(qreg_a)
+circ.measure(qreg_a,creg)
+with circ.if_test((creg,0)):
+    circ.swap(qreg_prev,qreg_next)
+# Draw the circuit
+circ.draw('mpl')
