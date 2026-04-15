8a8dcdaa92e8ac9e0b674441c566cb9d97343a60
Amelia Fargason <amelia.fargason@gmail.com>
2026-04-15 16:09:26 -0400

    WIP: integrating jacobian

--- circuit.py
+++ circuit.py
@@ -1,6 +1,8 @@
 from qiskit import QuantumRegister, QuantumCircuit, ClassicalRegister
 from qiskit.circuit.library import UnitaryGate
 import numpy as np
+from qiskit.primitives import StatevectorEstimator
+from qiskit_algorithms.gradients import ParamShiftEstimatorGradient
 
 # Create a circuit with a register of three qubits
 # change this to actual A later
@@ -12,6 +14,26 @@ v_0_norm = np.linalg.norm(v_0)
 n_0 = int(np.ceil(np.log2(m_0)))
 print(n_0)
 A = np.eye(2**(n_0*3))
+
+
+# not sure how to implement this into actual circuit
+# quantum jacobian?
+
+#subset of parameters used in dimension matching requirement (this would have to be given earlier as it changes)
+params = [] 
+# Define the estimator
+estimator = StatevectorEstimator(seed=42)
+# Define the gradient
+gradient = ParamShiftEstimatorGradient(estimator)
+
+# Evaluate the gradient of the circuits using parameter shift gradients
+pse_grad_result = gradient.run(qc, H, params).result().gradients
+
+
+
+
+# acceptance ratio
+alpha = 
 
 qreg_prev = QuantumRegister(n_0)
 qreg_next = QuantumRegister(n_0)
@@ -27,7 +49,8 @@ circ.measure(qreg_a,creg)
 with circ.if_test((creg,0)):
     circ.swap(qreg_prev,qreg_next)
 # Draw the circuit
-circ.decompose(reps=5).draw('mpl')
+circ.draw('mpl')
+#circ.decompose(reps=5).draw('mpl')
 
 # understand TD vs. RJ
 # draw the decomposed circuit to assess the complexity of state_preparation for large inputs (done)
