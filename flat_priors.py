9c0a6643466233e437a6ca64a66373e4e1e863dd
Amelia Fargason <amelia.fargason@gmail.com>
2026-05-10 16:01:24 -0400

    In progress: started list of priors

---
+++ flat_priors.py
@@ -0,0 +1,25 @@
+import random
+import numpy as np
+
+# information on parameters from https://journals.aps.org/prd/abstract/10.1103/PhysRevD.103.044006
+
+sampling_frequency = 0
+
+# intrinsic parameters
+t_0_min = 0
+t_0_max = 0
+f_0_min = 0
+
+Q = random.uniform(.1,40)
+phi_0 = random.uniform(0,2*np.pi)
+
+#nyquist frequency
+f_0_max = sampling_frequency/2
+
+
+# extrinsic parameters
+
+
+# number of wavelets
+D_min = 0
+D = uniform(D_min,100)
